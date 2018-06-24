a4f4920477a47d672c0cc5a95e4344300d3f498b
Teppei <>
2018-06-25 00:59:43 +0900

    なぜやり方が間違っているかはわかんなくてうざいけど、とりあえず銃声はなった

---
+++ Project-shooting/Assets/gun_sound.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gun_sound : MonoBehaviour {
+	public AudioClip gun;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetKeyDown (KeyCode.Z)){
+			GetComponent<AudioSource>().PlayOneShot(gun,1f);
+			}
+	}
+}
