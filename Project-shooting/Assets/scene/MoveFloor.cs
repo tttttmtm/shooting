f36e9ee63d652eca137b9fe3b14480d51099b86e
津村雄太 <ictmtm@icloud.com>
2018-06-27 14:38:37 +0900

    物動かした

---
+++ Project-shooting/Assets/scene/MoveFloor.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class MoveFloor : MonoBehaviour {
+
+	private Vector3 initialPosition;
+
+	void Start () {
+
+		initialPosition = transform.position;
+
+	}
+
+	void Update () {
+
+		transform.position = new Vector3(Mathf.Sin(Time.time) * 2.0f + initialPosition.x, Mathf.Sin(Time.time) * 2.0f+initialPosition.y, initialPosition.z);
+
+	}
+}
