820775eeceb80d55de5adbeb5604779740b9aa37
Teppei <>
2018-09-04 22:46:34 +0900

    なんか文字ぼやけるけど一応つけた

---
+++ Project-shooting/Assets/Script/gHP.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class gHP : MonoBehaviour {
+
+	public Text HPscore;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		HPscore.text = gcollision.gHP.ToString ();
+	}
+}
---
+++ Project-shooting/Assets/Script/gcollision.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class gcollision : MonoBehaviour {
+	public static int gHP;
+	// Use this for initialization
+	void Start () {
+		gHP = 10;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+	private void OnCollisionEnter( Collision i_collision ){
+		gHP--;
+	}
+}
