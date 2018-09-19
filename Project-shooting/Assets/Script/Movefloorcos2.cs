4b487b6b67afe822489243cdeaeb17df3be76a86
津村雄太 <ictmtm@icloud.com>
2018-09-19 22:33:55 +0900

    緑以外にランダムの動きさせた。行動範囲の制限方法の考え中

--- Project-shooting/Assets/Script/Movefloorcos2.cs
+++ Project-shooting/Assets/Script/Movefloorcos2.cs
@@ -11,36 +11,55 @@ public class Movefloorcos2 : MonoBehaviour {
 	private int count = 0;
 	private Vector3 nowPosition;
 	private int random = Random.Range(0, 4);
+	private float maxX, minX, maxZ, minZ;
 
 	void Start () {
 
 		initialPosition = transform.position;
+		maxX = initialPosition.x + 10.0f;
+		minX = initialPosition.x - 10.0f;
+		maxZ = initialPosition.z + 10.0f;
+		minZ = initialPosition.z - 10.0f;
 
 	}
 
 	void Update () {
 		if (count > 15) {
 			random = Random.Range (0, 4);
-			//initialPosition = transform.position;
 			count = 0;
 		}else{
 		if (hit < HP) {
-				if (random == 0) {
-					transform.position += transform.right * 20.0f * Time.deltaTime;
-				} else if (random == 1) {
-					transform.position += transform.forward * 20.0f * Time.deltaTime;
-				} else if (random == 2) {
-					transform.position -= transform.right * 20.0f * Time.deltaTime;
-				} else if (random == 3) {
-					transform.position -= transform.forward * 20.0f * Time.deltaTime;
-				}
-		} else {
-			float x = 90;
-			nowPosition = transform.position;
-			transform.position = new Vector3 (nowPosition.x, 0.39f, nowPosition.z);
-			transform.rotation = Quaternion.Euler (x, 0.0f, 0.0f);
+				nowPosition = transform.position;
+				if (count > 15) {
+					random = Random.Range (0, 4);
+					count = 0;
+				}else if (random == 0) {
+					if (maxX > nowPosition.x) {
+						transform.position += transform.right * 20.0f * Time.deltaTime;
+					}else transform.position -= transform.right * 30.0f * Time.deltaTime;
+
+					} else if (random == 1) {
+					if (maxZ > nowPosition.z) {
+						transform.position += transform.forward * 20.0f * Time.deltaTime;
+					}else transform.position -= transform.forward * 30.0f * Time.deltaTime;
+						
+					} else if (random == 2) {
+					if (minX > nowPosition.x) {
+						transform.position -= transform.right * 20.0f * Time.deltaTime;
+					}else transform.position += transform.right * 30.0f * Time.deltaTime;
+
+					} else if (random == 3) {
+					if (minZ > nowPosition.z) {
+						transform.position -= transform.forward * 20.0f * Time.deltaTime;
+					}else transform.position += transform.forward * 30.0f * Time.deltaTime;
+					}
+			} else {
+				float x = 90;
+				nowPosition = transform.position;
+				transform.position = new Vector3 (nowPosition.x, 0.39f, nowPosition.z);
+				transform.rotation = Quaternion.Euler (x, 0.0f, 0.0f);
+			}
 		}
-	}
 		count++;
 	}
 
