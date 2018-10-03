f69f53d40ebdb1c4e3173262de4feb5930e6d000
Teppei <>
2018-10-03 14:18:18 +0900

    とりあえずスペースを押したら最後のシーンに遷移するようにした

---
+++ Project-shooting/Assets/scene/final/final_score.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class final_score : MonoBehaviour {
+
+	public Text finalGUI;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		finalGUI.text = "Score : "+ s_shooting.score.ToString ();
+	}
+}
