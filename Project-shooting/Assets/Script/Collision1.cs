8b7a7faf202add8558d9068da74cc6fb4fc785ff
Teppei <>
2018-10-03 14:49:22 +0900

    名前変えた

--- Project-shooting/Assets/Script/Collision1.cs
+++ Project-shooting/Assets/Script/Collision1.cs
@@ -14,15 +14,17 @@ public class Collision1 : MonoBehaviour {
 		
 	}
 	void OnCollisionEnter(Collision collision) {
-		if (collision.gameObject.name == "E1") {
+		if (collision.gameObject.name == "Bunny_easy") {
 			s_shooting.score = s_shooting.score + 30;
-		} else if (collision.gameObject.name == "E2") {
+		} else if (collision.gameObject.name == "Bunny_hard") {
 			s_shooting.score = s_shooting.score + 50;
-		} else if (collision.gameObject.name == "E3") {
+		} else if (collision.gameObject.name == "zou_hard") {
 			s_shooting.score = s_shooting.score + 100;
-		} else if (collision.gameObject.name == "E4") {
+		} else if (collision.gameObject.name == "zou_easy") {
 			s_shooting.score = s_shooting.score + 10;
-		} else if (collision.gameObject.name == "E5") {
+		} else if (collision.gameObject.name == "ahiru") {
+			s_shooting.score = s_shooting.score + 5;
+		} else if (collision.gameObject.name == "piero") {
 			s_shooting.score = s_shooting.score + 5;
 		} else {
 			
