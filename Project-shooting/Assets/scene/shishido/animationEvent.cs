fc6220a1648d9fabd572febc52085e7b124f075e
Teppei <>
2018-10-05 10:24:06 +0900

    直ったかも

---
+++ Project-shooting/Assets/scene/shishido/animationEvent.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class animationEvent : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void StartSinking(){
+		
+}
+}
