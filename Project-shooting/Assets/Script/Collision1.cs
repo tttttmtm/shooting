b6a7219da7f0c89819b6b633cc3552d7bf093f56
津村雄太 <ictmtm@icloud.com>
2018-10-09 14:40:29 +0900

    象のリメイク

--- Project-shooting/Assets/Script/Collision1.cs
+++ Project-shooting/Assets/Script/Collision1.cs
@@ -23,26 +23,40 @@ public class Collision1 : MonoBehaviour {
 	}
 	void OnCollisionEnter(Collision collision) {
 		if (collision.gameObject.name == "Bunny_easy") {
-			s_shooting.score = s_shooting.score + 30;
-			Bunny_easy = true;
+			if (Bunny_easy == false) {
+				s_shooting.score = s_shooting.score + 30;
+				Bunny_easy = true;
+			}
 		} else if (collision.gameObject.name == "Bunny_hard") {
-			s_shooting.score = s_shooting.score + 50;
-			Bunny_hard = true;
+			if (Bunny_hard == false) {
+				s_shooting.score = s_shooting.score + 50;
+				Bunny_hard = true;
+			}
 		} else if (collision.gameObject.name == "zou_hard") {
-			s_shooting.score = s_shooting.score + 100;
-			zou_hard = true;
+			if (zou_hard == false) {
+				s_shooting.score = s_shooting.score + 100;
+				zou_hard = true;
+			}
 		} else if (collision.gameObject.name == "zou_easy") {
-			s_shooting.score = s_shooting.score + 10;
-			zou_easy = true;
+			if (zou_easy == false) {
+				s_shooting.score = s_shooting.score + 10;
+				zou_easy = true;
+			}
 		} else if (collision.gameObject.name == "ahiru") {
-			s_shooting.score = s_shooting.score + 5;
-			ahiru = true;
+			if (ahiru == false) {
+				s_shooting.score = s_shooting.score + 5;
+				ahiru = true;
+			}
 		} else if (collision.gameObject.name == "piero") {
-			s_shooting.score = s_shooting.score + 5;
-			piero = true;
+			if (piero == false) {
+				s_shooting.score = s_shooting.score + 5;
+				piero = true;
+			}
 		}else if (collision.gameObject.name == "Bunny_metal") {
-			s_shooting.score = s_shooting.score + 100;
-			Bunny_metal = true;
+			if (Bunny_metal == false) {
+				s_shooting.score = s_shooting.score + 100;
+				Bunny_metal = true;
+			}
 		} else {
 			
 		}
