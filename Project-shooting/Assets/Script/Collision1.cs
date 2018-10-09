1582273436b25a8ef92cc30f28f937e52383127c
Teppei <>
2018-10-09 10:46:26 +0900

    わからん

--- Project-shooting/Assets/Script/Collision1.cs
+++ Project-shooting/Assets/Script/Collision1.cs
@@ -5,6 +5,14 @@ using UnityEngine;
 public class Collision1 : MonoBehaviour {
 	// Use this for initialization
 
+	public static bool Bunny_easy=false;
+	public static bool Bunny_hard=false;
+	public static bool zou_easy=false;
+	public static bool zou_hard=false;
+	public static bool ahiru=false;
+	public static bool piero=false;
+	public static bool Bunny_metal=false;
+
 	void Start () {
 		Debug.Log ("スタートしました");
 	}
@@ -16,16 +24,25 @@ public class Collision1 : MonoBehaviour {
 	void OnCollisionEnter(Collision collision) {
 		if (collision.gameObject.name == "Bunny_easy") {
 			s_shooting.score = s_shooting.score + 30;
+			Bunny_easy = true;
 		} else if (collision.gameObject.name == "Bunny_hard") {
 			s_shooting.score = s_shooting.score + 50;
+			Bunny_hard = true;
 		} else if (collision.gameObject.name == "zou_hard") {
 			s_shooting.score = s_shooting.score + 100;
+			zou_hard = true;
 		} else if (collision.gameObject.name == "zou_easy") {
 			s_shooting.score = s_shooting.score + 10;
+			zou_easy = true;
 		} else if (collision.gameObject.name == "ahiru") {
 			s_shooting.score = s_shooting.score + 5;
+			ahiru = true;
 		} else if (collision.gameObject.name == "piero") {
 			s_shooting.score = s_shooting.score + 5;
+			piero = true;
+		}else if (collision.gameObject.name == "Bunny_metal") {
+			s_shooting.score = s_shooting.score + 100;
+			Bunny_metal = true;
 		} else {
 			
 		}
---
+++ Project-shooting/Assets/scene/final/make.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class make : MonoBehaviour {
+
+	public GameObject Bunny_hard;
+	public GameObject Bunny_easy;
+	public GameObject Bunny_metal;
+	public GameObject zou_hard;
+	public GameObject zou_easy;
+	public GameObject piero;
+	public GameObject ahiru;
+
+	// Use this for initialization
+	void Start () {
+		if(Collision1.Bunny_hard = true) make_Bunny_hard();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+	void make_Bunny_hard(){
+		GameObject Bunny_hard = GameObject.Instantiate (Bunny_hard) as GameObject;
+	}
+}
