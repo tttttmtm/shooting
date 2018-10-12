482fe4c685f3324d4318a18b054aa3a0abbfa05e
Teppei <>
2018-10-12 13:32:07 +0900

    時間

--- Project-shooting/Assets/Script/Score_out.cs
+++ Project-shooting/Assets/Script/Score_out.cs
@@ -7,21 +7,31 @@ public class Score_out : MonoBehaviour
 {
 	public Text scoreGUI;
 	public int time = 120;
+	public int minute;
+	public float seconds;
+	public float oldseconds;
 
 	// Use this for initialization
 	void Start ()
 	{
-		
+		minute = 2;
+		seconds = 0f;
+		oldseconds = 0f;
 	}
 	
 	// Update is called once per frame
 	void Update ()
 	{
-		Invoke("text",5);
+		seconds = seconds - Time.deltaTime;
+		if(seconds < 0f) {
+			minute--;
+			seconds = seconds + 59f;
+		}
+		//　値が変わった時だけテキストUIを更新
+		if((int)seconds != (int)oldseconds) {
+			scoreGUI.text = "残り時間 : " + minute.ToString("00") + ":" + ((int) seconds).ToString ("00");
+		}
+		oldseconds = seconds;
 	}
-
-	void text(){
-		scoreGUI.text = "残り時間 : "+ time.ToString ();
-		time = time - 1;
-	}
+	
 }
